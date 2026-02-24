4e8fdbae48a9b7be55cdf7c244ece122ae3eeb88
therealityreport <admin@thereality.report>
2026-02-24 12:15:24 -0500

    workspace: enforce admin host env defaults and narrow tab refresh matching

--- scripts/open-or-refresh-browser-tab.sh
+++ scripts/open-or-refresh-browser-tab.sh
@@ -14,10 +14,6 @@ if [[ -z "$URL_NO_SLASH" ]]; then
   URL_NO_SLASH="$URL"
 fi
 URL_WITH_SLASH="${URL_NO_SLASH}/"
-TARGET_PORT=""
-if [[ "$URL_NO_SLASH" =~ ^https?://[^/:]+:([0-9]+)(/.*)?$ ]]; then
-  TARGET_PORT="${BASH_REMATCH[1]}"
-fi
 
 ALT_URL_NO_SLASH=""
 ALT_URL_WITH_SLASH=""
@@ -50,16 +46,14 @@ refresh_chrome_tabs() {
   local url_with_slash="$3"
   local alt_url_no_slash="${4:-}"
   local alt_url_with_slash="${5:-}"
-  local target_port="${6:-}"
 
-  osascript - "$target_url" "$url_no_slash" "$url_with_slash" "$alt_url_no_slash" "$alt_url_with_slash" "$target_port" 2>/dev/null <<'APPLESCRIPT' || return 1
+  osascript - "$target_url" "$url_no_slash" "$url_with_slash" "$alt_url_no_slash" "$alt_url_with_slash" 2>/dev/null <<'APPLESCRIPT' || return 1
 on run argv
   set targetURL to item 1 of argv
   set targetURLNoSlash to item 2 of argv
   set targetURLWithSlash to item 3 of argv
   set altURLNoSlash to item 4 of argv
   set altURLWithSlash to item 5 of argv
-  set targetPort to item 6 of argv
 
   tell application "Google Chrome"
     activate
@@ -77,7 +71,7 @@ on run argv
       repeat with tabIndex from 1 to (count of tabs of aWindow)
         set aTab to tab tabIndex of aWindow
         set tabURL to URL of aTab
-        if my urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash, targetPort) then
+        if my urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash) then
           tell aTab to reload
           set refreshedCount to refreshedCount + 1
           if firstWindowIndex is 0 then
@@ -98,7 +92,7 @@ on run argv
   end tell
 end run
 
-on urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash, targetPort)
+on urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash)
   if tabURL is missing value then
     return false
   end if
@@ -116,68 +110,8 @@ on urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoS
       return true
     end if
   end if
-
-  -- APP/Admin family: treat localhost, 127.0.0.1, and *.localhost on :3000 as equivalent.
-  if targetPort is "3000" then
-    set targetHostPort to my hostPortFromURL(targetURLNoSlash)
-    set tabHostPort to my hostPortFromURL(tabURL)
-    if (targetHostPort is not "") and (tabHostPort is not "") then
-      set targetHost to my hostFromHostPort(targetHostPort)
-      set targetPortParsed to my portFromHostPort(targetHostPort)
-      set tabHost to my hostFromHostPort(tabHostPort)
-      set tabPortParsed to my portFromHostPort(tabHostPort)
-      if (targetPortParsed is "3000") and (tabPortParsed is "3000") then
-        if (my isLocalhostFamilyHost(targetHost)) and (my isLocalhostFamilyHost(tabHost)) then
-          return true
-        end if
-      end if
-    end if
-  end if
-
   return false
 end urlMatches
-
-on hostPortFromURL(urlText)
-  set schemeOffset to offset of "://" in urlText
-  if schemeOffset is 0 then
-    return ""
-  end if
-  set noScheme to text (schemeOffset + 3) thru -1 of urlText
-  set slashOffset to offset of "/" in noScheme
-  if slashOffset is 0 then
-    return noScheme
-  end if
-  return text 1 thru (slashOffset - 1) of noScheme
-end hostPortFromURL
-
-on hostFromHostPort(hostPortText)
-  set colonOffset to offset of ":" in hostPortText
-  if colonOffset is 0 then
-    return hostPortText
-  end if
-  return text 1 thru (colonOffset - 1) of hostPortText
-end hostFromHostPort
-
-on portFromHostPort(hostPortText)
-  set colonOffset to offset of ":" in hostPortText
-  if colonOffset is 0 then
-    return ""
-  end if
-  return text (colonOffset + 1) thru -1 of hostPortText
-end portFromHostPort
-
-on isLocalhostFamilyHost(hostText)
-  if hostText is "localhost" then
-    return true
-  end if
-  if hostText is "127.0.0.1" then
-    return true
-  end if
-  if hostText ends with ".localhost" then
-    return true
-  end if
-  return false
-end isLocalhostFamilyHost
 APPLESCRIPT
 }
 
@@ -187,16 +121,14 @@ refresh_safari_tabs() {
   local url_with_slash="$3"
   local alt_url_no_slash="${4:-}"
   local alt_url_with_slash="${5:-}"
-  local target_port="${6:-}"
 
-  osascript - "$target_url" "$url_no_slash" "$url_with_slash" "$alt_url_no_slash" "$alt_url_with_slash" "$target_port" 2>/dev/null <<'APPLESCRIPT' || return 1
+  osascript - "$target_url" "$url_no_slash" "$url_with_slash" "$alt_url_no_slash" "$alt_url_with_slash" 2>/dev/null <<'APPLESCRIPT' || return 1
 on run argv
   set targetURL to item 1 of argv
   set targetURLNoSlash to item 2 of argv
   set targetURLWithSlash to item 3 of argv
   set altURLNoSlash to item 4 of argv
   set altURLWithSlash to item 5 of argv
-  set targetPort to item 6 of argv
 
   tell application "Safari"
     activate
@@ -215,7 +147,7 @@ on run argv
       repeat with aTab in tabs of aWindow
         set tabCounter to tabCounter + 1
         set tabURL to URL of aTab
-        if my urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash, targetPort) then
+        if my urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash) then
           do JavaScript "window.location.reload();" in aTab
           set refreshedCount to refreshedCount + 1
           if firstWindowIndex is 0 then
@@ -236,7 +168,7 @@ on run argv
   end tell
 end run
 
-on urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash, targetPort)
+on urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoSlash, altURLWithSlash)
   if tabURL is missing value then
     return false
   end if
@@ -254,68 +186,8 @@ on urlMatches(tabURL, targetURL, targetURLNoSlash, targetURLWithSlash, altURLNoS
       return true
     end if
   end if
-
-  -- APP/Admin family: treat localhost, 127.0.0.1, and *.localhost on :3000 as equivalent.
-  if targetPort is "3000" then
-    set targetHostPort to my hostPortFromURL(targetURLNoSlash)
-    set tabHostPort to my hostPortFromURL(tabURL)
-    if (targetHostPort is not "") and (tabHostPort is not "") then
-      set targetHost to my hostFromHostPort(targetHostPort)
-      set targetPortParsed to my portFromHostPort(targetHostPort)
-      set tabHost to my hostFromHostPort(tabHostPort)
-      set tabPortParsed to my portFromHostPort(tabHostPort)
-      if (targetPortParsed is "3000") and (tabPortParsed is "3000") then
-        if (my isLocalhostFamilyHost(targetHost)) and (my isLocalhostFamilyHost(tabHost)) then
-          return true
-        end if
-      end if
-    end if
-  end if
-
   return false
 end urlMatches
-
-on hostPortFromURL(urlText)
-  set schemeOffset to offset of "://" in urlText
-  if schemeOffset is 0 then
-    return ""
-  end if
-  set noScheme to text (schemeOffset + 3) thru -1 of urlText
-  set slashOffset to offset of "/" in noScheme
-  if slashOffset is 0 then
-    return noScheme
-  end if
-  return text 1 thru (slashOffset - 1) of noScheme
-end hostPortFromURL
-
-on hostFromHostPort(hostPortText)
-  set colonOffset to offset of ":" in hostPortText
-  if colonOffset is 0 then
-    return hostPortText
-  end if
-  return text 1 thru (colonOffset - 1) of hostPortText
-end hostFromHostPort
-
-on portFromHostPort(hostPortText)
-  set colonOffset to offset of ":" in hostPortText
-  if colonOffset is 0 then
-    return ""
-  end if
-  return text (colonOffset + 1) thru -1 of hostPortText
-end portFromHostPort
-
-on isLocalhostFamilyHost(hostText)
-  if hostText is "localhost" then
-    return true
-  end if
-  if hostText is "127.0.0.1" then
-    return true
-  end if
-  if hostText ends with ".localhost" then
-    return true
-  end if
-  return false
-end isLocalhostFamilyHost
 APPLESCRIPT
 }
 
@@ -326,13 +198,13 @@ if [[ "$(uname)" != "Darwin" ]]; then
 fi
 
 if command -v osascript >/dev/null 2>&1; then
-  refreshed_count="$(refresh_chrome_tabs "$URL" "$URL_NO_SLASH" "$URL_WITH_SLASH" "$ALT_URL_NO_SLASH" "$ALT_URL_WITH_SLASH" "$TARGET_PORT" || true)"
+  refreshed_count="$(refresh_chrome_tabs "$URL" "$URL_NO_SLASH" "$URL_WITH_SLASH" "$ALT_URL_NO_SLASH" "$ALT_URL_WITH_SLASH" || true)"
   if [[ "$refreshed_count" =~ ^[0-9]+$ ]] && (( refreshed_count > 0 )); then
     echo "[open-or-refresh-browser-tab] Refreshed ${refreshed_count} existing Chrome tab(s): ${LABEL} -> ${URL}"
     exit 0
   fi
 
-  refreshed_count="$(refresh_safari_tabs "$URL" "$URL_NO_SLASH" "$URL_WITH_SLASH" "$ALT_URL_NO_SLASH" "$ALT_URL_WITH_SLASH" "$TARGET_PORT" || true)"
+  refreshed_count="$(refresh_safari_tabs "$URL" "$URL_NO_SLASH" "$URL_WITH_SLASH" "$ALT_URL_NO_SLASH" "$ALT_URL_WITH_SLASH" || true)"
   if [[ "$refreshed_count" =~ ^[0-9]+$ ]] && (( refreshed_count > 0 )); then
     echo "[open-or-refresh-browser-tab] Refreshed ${refreshed_count} existing Safari tab(s): ${LABEL} -> ${URL}"
     exit 0
